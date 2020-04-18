9b03547109858b47bdfe3b89c3280561e0fa9158
Gustavo Cotta <gmcotta34@gmail.com>
2020-04-18 12:35:05 -0300

    Transaction and Category Models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('tramsactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('integer')
   value: number;
 
+  @OneToOne(() => Category)
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
